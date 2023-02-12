6f8c25595accd64363baff77ef4a96efa638526f
Egor <rabotko2017@yandex.ru>
2023-02-12 16:00:14 +0300

    Initial Commit

---
+++ toDoV1/toDoV1App.swift
@@ -0,0 +1,17 @@
+//
+//  toDoV1App.swift
+//  toDoV1
+//
+//  Created by estend on 12.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct toDoV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
